1483c1e964f922edd60a1994982b6eb61524fcba
Soelferwulf <soelferwulf@gmail.com>
2025-03-13 11:03:27 +0100

    first try with API

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,3 +1,4 @@
+import { formatDynamicAPIAccesses } from "next/dist/server/app-render/dynamic-rendering";
 import Link from "next/link";
 
 const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
@@ -12,6 +13,12 @@ const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
   }
 
   return <button>Login</button>;
+
+  // 1. Create a /login page
+
+  // 2. Create an email/password form
+
+  // 3. Make a POST request to /api/auth
 };
 
 export default ButtonLogin;
