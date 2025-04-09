9fab393bf73224cfa099eceacb075664a3930e6b
Soelferwulf <soelferwulf@gmail.com>
2025-04-09 16:05:32 +0200

    custom boards and board links

---
+++ app/dashboard/b/[boardId]/page.js
@@ -0,0 +1,31 @@
+import { redirect } from "next/navigation";
+import connectMongo from "@/libs/mongoose";
+import Board from "@/models/Board";
+import { auth } from "@/auth";
+
+const getBoard = async (boardId) => {
+  const session = await auth();
+
+  await connectMongo();
+
+  const board = await Board.findOne({
+    _id: boardId,
+    userId: session?.user?.id,
+  });
+
+  if (!board) {
+    redirect("/dashboard");
+  }
+
+  return board;
+};
+
+export default async function FeedbackBoard({ params }) {
+  const { boardId } = params;
+
+  // Get board with ObjectID 'boardId'
+
+  const board = await getBoard(boardId);
+
+  return <main>{board.name}</main>;
+}
